e7b41a26a1602432d518c7a2a8aeece545e29f60
Brandon Lee <brandonlee781@gmail.com>
2021-07-03 14:55:00 -0500

    remove unused code for now

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import styled from 'styled-components'
+import { useParams } from 'react-router-dom'
+// import styled from 'styled-components'
 
 import { ListItem, List } from '../../components/SkillWrapper/SkillWrapper'
 import SkillListItem from '../../components/SkillListItem/SkillListItem'
@@ -8,23 +8,23 @@ import { useRanks } from '../../context/ranks.context'
 
 import { combatSkills, bioticSkills, techSkills } from '../../data/skills'
 
-const ListLink = styled(Link)`
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: center;
-  justify-content: flex-end;
-  width: 100%;
-  height: 100%;
-  text-align: right;
-  color: #51c2bc;
-  margin-right: 4px;
-  text-transform: uppercase;
-  font-weight: 500;
+// const ListLink = styled(Link)`
+//   display: flex;
+//   flex-flow: row nowrap;
+//   align-items: center;
+//   justify-content: flex-end;
+//   width: 100%;
+//   height: 100%;
+//   text-align: right;
+//   color: #51c2bc;
+//   margin-right: 4px;
+//   text-transform: uppercase;
+//   font-weight: 500;
 
-  &:hover {
-    color: white;
-  }
-`
+//   &:hover {
+//     color: white;
+//   }
+// `
 
 const Main = (): JSX.Element => {
   const { urlString } = useParams<{ urlString?: string }>()
